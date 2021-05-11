4b58807ee265132ba28d835429770182d96421e5
Mark Boyle <markboyle19@gmail.com>
2021-05-11 10:19:52 +1000

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# puts 'Destroying old records'
+# User.destroy.all
+# puts 'Seeding started'
+
+user1 = User.create(username: 'Bob', email: 'bob@email.com', password: 'bobbobbob')
+
+
+
+10.times do 
+    Celebrity.create(name: Faker::Artist.name)
+end
